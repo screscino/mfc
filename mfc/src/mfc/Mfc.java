febc2ac150ab2e36508d191b6454a549460e825f
screscino <screscino@gmail.com>
2025-01-23 23:07:57 +0100

    Update Mfc.java

--- mfc/src/mfc/Mfc.java
+++ mfc/src/mfc/Mfc.java
@@ -14,6 +14,7 @@ public class Mfc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("ciao");
         // TODO code application logic here
     }
     
