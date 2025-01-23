feadc77b0c055711bf85f75d77874b90a6b7617b
utente <utente@screscino-himalayas.nord>
2025-01-23 23:09:40 +0100

    2

--- mfc/src/mfc/Mfc.java
+++ mfc/src/mfc/Mfc.java
@@ -14,7 +14,7 @@ public class Mfc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ciao");
+        System.out.println("ciaop");
         // TODO code application logic here
     }
     
