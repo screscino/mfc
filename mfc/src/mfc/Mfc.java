314da45b3ad8339263dbcb75f060631884967ad6
screscino <screscino@gmail.com>
2025-01-28 08:10:01 +0100

    primo

--- mfc/src/mfc/Mfc.java
+++ mfc/src/mfc/Mfc.java
@@ -15,7 +15,7 @@ public class Mfc {
         
         
 System.out.println("cp");
-       System.out.println("cp");
+       System.out.println("prova");
        
        
        System.out.println("cp");
